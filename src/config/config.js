b94c308bb15396da2a67842f34ec0b0feaf7a372
Vaibhav <110817406+thevaibhavrajput@users.noreply.github.com>
2025-10-05 22:39:54 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  serverUrl: import.meta.env.VITE_APP_SERVERURL || "https://34.131.209.110/api/v1",
+  serverUrl: import.meta.env.VITE_APP_SERVERURL || "http://34.131.209.110/api/v1",
   localUrl: import.meta.env.VITE_APP_LOCALURL || "http://localhost:3030/api/v1",
   proxyUrl: import.meta.env.VITE_APP_PROXYURL,
 };
